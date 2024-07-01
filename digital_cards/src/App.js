a6b61ee02c64df9b95a3dc54510e005cbfca2868
DanielRPrado <ed.prado0607@gmail.com>
2024-07-01 10:05:28 -0600

    Create the project skeleton

---
+++ digital_cards/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from "./pages/Home";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/login" component={Home}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
